66c3794d27541aa20dc4b954c8465b3013ddb6eb
Vladyslav Shynkarenko <vadic7wee2@gmail.com>
2015-04-01 15:42:52 +0300

    Getters and setters examples

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,7 +5,6 @@ import caseclasses.CaseClasses
 import inheritance.Spiral
 import traits.{Queues, Traits}
 
-import scala.collection.mutable
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
 object Main {
@@ -21,6 +20,7 @@ object Main {
     traits
     inheritance
     importsAndPackages
+    gettersAndSetters
   }
 
   def rationalNumbers {
@@ -547,5 +547,24 @@ object Main {
   def importsAndPackages {
     import modulatiry.navigation.menu.Navigation
     Navigation.show
+  }
+
+  def gettersAndSetters{
+    var t1, t2 = new Time
+    t1.hour = 12 // set hour to 12
+    t1.minute = 10 // set minute to 10
+    println("Get time using getters: " +t1.hour+" : "+t1.minute) // get hour and minute
+  }
+
+  class Time {
+    private var h: Int = _ // assigns default value to variable. For Itn - 0, for boolean - false, for reference - null
+    private var m: Int = _
+
+    def hour: Int = h// getter
+    def hour_=(x: Int) {h = x}// setter
+
+    def minute: Int = m
+    def minute_=(x: Int) {m = x}
+
   }
 }
